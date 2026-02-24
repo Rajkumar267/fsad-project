b0d28351bc72ddfea80db5c9e116ca99327104f0
Rajkumar267 <oliverajkumar1509@gmail.com>
2026-02-24 18:38:25 +0530

    Setup for GitHub Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom' // 👈 changed
 import { AuthProvider } from './context/AuthContext.jsx'
 import { ThemeProvider } from './context/ThemeContext.jsx'
 import { RatingsProvider } from './context/RatingsContext.jsx'
@@ -9,7 +9,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter> {/* 👈 changed */}
       <ThemeProvider>
         <AuthProvider>
           <RatingsProvider>
@@ -17,6 +17,6 @@ createRoot(document.getElementById('root')).render(
           </RatingsProvider>
         </AuthProvider>
       </ThemeProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
